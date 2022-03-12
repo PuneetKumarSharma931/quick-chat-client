c3618fc8a6791f89d7a52081919df10e037aa6dc
Puneet Kumar Sharma <puneetkumarsharma@Puneets-Air.domain.name>
2022-03-12 18:14:23 +0530

    added popup modal for name and made website responsive

--- static/js/client.js
+++ static/js/client.js
@@ -6,11 +6,43 @@ let messageContainer = document.querySelector(".container");
 
 let audioElement = new Audio('static/media/sound.mp3');
 
-const name = prompt('Enter your name:');
+const popup = document.getElementsByClassName('popup')[0];
+const join = document.getElementsByClassName('joinBtn')[0];
+const input = document.getElementById('name');
+const chats = document.getElementsByClassName('chatRoom')[0];
+let name;
 
-socket.emit('new-user-joined', name);
+const getName = new Promise((resolve, reject) => {
 
-const append = (user,message,position)=>{
+    window.onload = () => {
+
+        popup.style.display = 'block';
+        chats.classList.toggle('active');
+
+        join.addEventListener('click', () => {
+
+            name = input.value;
+            popup.style.display = 'none';
+            chats.classList.toggle('active');
+        });
+    }
+
+    setInterval(() => {
+        
+        if(name!=undefined)
+        resolve();
+
+    }, 200);
+});
+
+
+getName.then(() => {
+    socket.emit('new-user-joined', name);
+}).catch(() => {
+    console.log("Some Error Occured!");
+});
+
+const append = (user, message, position) => {
 
     let chat = document.createElement('div');
     chat.classList.add('message');
@@ -18,54 +50,58 @@ const append = (user,message,position)=>{
     chat.innerHTML = `<strong>${user}</strong>: ${message}`;
     messageContainer.appendChild(chat);
 
-    if(user!='You') {
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+
+    if (user != 'You') {
 
         audioElement.play();
     }
 }
 
-const appendJoined = (user)=>{
+const appendJoined = (user) => {
 
     let chat = document.createElement('div');
     chat.classList.add('message');
     chat.classList.add('left');
     chat.innerHTML = `<strong>${user}</strong> joined the chat`;
     messageContainer.appendChild(chat);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
     audioElement.play();
 }
 
-const appendLeft = (user)=>{
+const appendLeft = (user) => {
 
     let chat = document.createElement('div');
     chat.classList.add('message');
     chat.classList.add('left');
     chat.innerHTML = `<strong>${user}</strong> left the chat`;
     messageContainer.appendChild(chat);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
     audioElement.play();
 }
 
-socket.on('user-joined', name=>{
+socket.on('user-joined', name => {
 
     appendJoined(name);
 });
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
 
     e.preventDefault();
 
     let messageInp = message.value;
 
-    append('You',messageInp,'right');
+    append('You', messageInp, 'right');
     message.value = "";
     socket.emit('send', messageInp);
 });
 
-socket.on('received', data=>{
+socket.on('received', data => {
 
     append(data.name, data.message, 'left');
 });
 
-socket.on('left', name=>{
+socket.on('left', name => {
 
     appendLeft(name);
 });
